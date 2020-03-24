31ed7fe8e6b593246b75fa7fe37878e5ea316bc0
root <root@instance-1.c.ancient-flag-212804.internal>
2020-03-24 19:44:02 +0000

    server setup complete

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-const fs = require('fs');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const express = require('express')
+const app = express();
 
-const collegeCard = "https://api.data.gov/ed/collegescorecard/v1/schools.json?api_key=6PKYnV4rI5tGks4tAm0s0KKHTQLxON1Jf8hX6bU8&school.name=Stony+Brook+University&fields=id,school.name,latest.admissions.admission_rate.overall";
-
-let getRanking = html => {
-    const $ = cheerio.load(html);
-    ranks = [];
-    ranks.push($('span.institution-info__ranking-number').text().substring(1));
-    console.log(ranks);
-}
-
-
-fs.readFile('./colleges.txt',{encoding: 'utf8'}, (err,data) => {
-    const colleges = data.split('\n');
-    axios.get("https://www.timeshighereducation.com/world-university-rankings/washington-jefferson-college")
-        .then(response => {
-            getRanking(response.data);
-    })
-    .catch(error => {
-        console.log(error)
-    })
+app.get('/', (req, res) => {
+  res.send('Hello BEAM boys!');
 });
 
-
+app.listen(5000,"127.0.0.1");
 
 
 
