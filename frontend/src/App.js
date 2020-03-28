2942db5f8b07d2f78d8a05c332aee3b6e9a202ad
mazhu <matthew.zhu@stonybrook.edu>
2020-03-28 10:35:23 -0400

    fix app.js v0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import Navbar from "./components/navbar.com
+import Navbar from "./components/Header";
+import homepage from "./components/homepage";
+import searchcollege from "./components/searchcollege";
 function App() {
   return (
 	<Router>
